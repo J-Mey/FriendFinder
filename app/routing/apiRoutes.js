5ffe3d532e3814adcdd78d7e4625a83f87fa84c0
J <j.mey.r8@gmail.com>
2019-07-19 00:30:02 -0400

    still updating api route

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,18 @@
-var friendsArray = require("../data/friends");
+var friends = require("../data/friends");
 
 module.exports = function(app) {
 
 app.get("/api/friends", function(req, res){
-    res.json(friendsArray);
+    res.json(friends);
 });
 
 app.post("api/friends", function(req, res){
+    var user = req.body;
+
+    for (var i = 0; i < user.scores.length; i++) {
+        user.scores[i] = parseInt(user.scores[i]);
+    }
+
     friends.push(req.body);
     res.json(true);
 });
