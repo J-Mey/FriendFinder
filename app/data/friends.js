c5a766edf7f9be0ee0a0c8e2f4a117396bfd7376
J <j.mey.r8@gmail.com>
2019-07-20 19:46:37 -0400

    updated friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,22 +1,22 @@
 var friendsArray = [
 {"name": "Robert Downey Jr.",
 "photo": "http://www.thelegendarytrend.com/wp-content/uploads/2016/05/RobertDowneyjr11-400x400.jpg",
-"scores": [5,1,4,4,5,1,2,5,4,1]
+"scores": [5,3,4,5,3,4,2,2,4,4]
 },
 
 {"name": "Chris Hemsworth",
 "photo": "https://bloximages.chicago2.vip.townnews.com/celebretainment.com/content/tncms/assets/v3/editorial/6/10/6100c7e9-2a68-5e55-b2ab-20a526008a43/5a56162da28ec.image.jpg?resize=400%2C400",
-"scores": [5,1,4,4,5,1,2,5,4,1]
+"scores": [4,1,4,5,4,3,4,1,5,5]
 },
 
 {"name": "Ali Wong",
 "photo": "https://d1qxviojg2h5lt.cloudfront.net/images/01DCQ4YT5WVZRRM5C2SAKZ7HYP/aliwong400.png",
-"scores": [5,1,4,4,5,1,2,5,4,1]
+"scores": [5,1,2,2,5,3,2,5,5,2]
 },
 
 {"name": "Cam Newton",
 "photo": "https://pbs.twimg.com/profile_images/671751328398487553/tNPkqu4v.jpg",
-"scores": [5,1,4,4,5,1,2,5,4,1]
+"scores": [5,5,4,5,4,5,4,2,5,4]
 }
 
 ]
